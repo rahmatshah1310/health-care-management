614683b9267646008ca8a54ca78c5dca5dc75db8
rahmatshah1310 <rahmatshah1310@gmail.com>
2026-03-23 22:46:06 +0500

    feat: Implement initial user and admin management including models, controllers, services, routes, authentication, and validation.

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ const { testConnection: sequelizeConnection } = require("./config/sequelize");
 const session = require("express-session");
 const { error } = require("console");
 
-var app = express();
+const loggerMiddleware = require("./middlewares/logger.mw");
+const responseMiddleware = require("./middlewares/response.mw");
 
+var app = express();
+app.use(responseMiddleware);
 sequelizeConnection();
 
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(express.json({ limit: "1mb" }));
 
+app.use(loggerMiddleware);
 app.use(cors());
 
 app.use(
@@ -32,6 +36,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index", { title: "Health Care Management" });
 });
+app.use("/api/v1", require("./routes/index"));
 
 app.use(function (req, res, next) {
   next(createError);
